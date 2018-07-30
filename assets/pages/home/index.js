8a5d57d816611dc92bc20563560a726048ec95b5
Rick li <285699093@qq.com>
2018-07-30 22:53:11 +0800

    先随便写点

--- assets/pages/home/index.js
+++ assets/pages/home/index.js
@@ -1,7 +1,19 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
+const Main = () => (
+  <article>
+    <header>不知道用来干啥练手 就写个博客功能吧 ：）</header>
+    <ul>
+      <li>用户管理</li>
+      <li>写文章</li>
+      <li>展示文章</li>
+      <li>首页就用来展示吧先</li>
+    </ul>
+  </article>
+)
+
 ReactDOM.render(
-  <h1>Welcome to home page</h1>,
+  <Main />,
   document.getElementById('app')
 );

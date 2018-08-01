0a4ed03f8d2f3c60e314796f9de22fb2b9d61201
Rick li <285699093@qq.com>
2018-08-01 22:45:55 +0800

    userbar 格式

--- assets/pages/home/UserBar.js
+++ assets/pages/home/UserBar.js
@@ -1,13 +1,21 @@
-/**
- * 负责用户登录状态展示
- * 已登录：展示头像 写文章
- * 头像引导向其他东西
- * 未登录：显示登录注册
- */
 import React from 'react';
 
-const UserBar = () => (
-  <div>UserBar占位符</div>
+const UserBar = ({ logined }) => (
+  logined ? <Avatar /> : <PreLogin />
+);
+
+const Avatar = () => (
+  <div>
+    <img src="" alt=""/>
+    <div className="go-write-btn">写文章</div>
+  </div>
 )
+
+const PreLogin = () => (
+  <div>
+    <div>登录</div>
+    <div>注册</div>
+  </div>
+);
 
 export default UserBar;

1c4ebb292b70dbac4dbb9a2d008ee9f84a9a2a04
g4rick <285699093@qq.com>
2018-08-12 21:32:21 +0800

    使用自己的eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 // todo: 太麻烦了 明天创建一个自己的eslint
 module.exports = {
-  "extends": "airbnb",
-  "plugins": [
-    "react",
-    "jsx-a11y",
-    "import"
-  ],
-  "rules": {
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    "react/prop-types": 0
-  }
+  "extends": "eslint-config-g4rick"
+  // "plugins": [
+  //   "react",
+  //   "jsx-a11y",
+  //   "import"
+  // ],
+  // "rules": {
+  //   "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  //   "react/prop-types": 0
+  // }
 };

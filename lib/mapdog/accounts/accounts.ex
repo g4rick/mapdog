6e1856dedba7a78df10ae554823d71f800699282
g4rick <285699093@qq.com>
2018-09-21 17:25:32 +0800

    create_user & update_user

--- lib/mapdog/accounts/accounts.ex
+++ lib/mapdog/accounts/accounts.ex
@@ -6,7 +6,7 @@ defmodule Mapdog.Accounts do
   import Ecto.Query, warn: false
   alias Mapdog.Repo
 
-  alias Mapdog.Accounts.User
+  alias Mapdog.Accounts.{User, Credential}
 
   @doc """
   Returns the list of users.
@@ -58,6 +58,7 @@ defmodule Mapdog.Accounts do
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:credential, with: &Credential.changeset/2)
     |> Repo.insert()
   end
 
@@ -76,6 +77,7 @@ defmodule Mapdog.Accounts do
   def update_user(%User{} = user, attrs) do
     user
     |> User.changeset(attrs)
+    |> Ecto.Changeset.cast_assoc(:credential, with: &Credential.changeset/2)
     |> Repo.update()
   end
 
@@ -107,8 +109,6 @@ defmodule Mapdog.Accounts do
   def change_user(%User{} = user) do
     User.changeset(user, %{})
   end
-
-  alias Mapdog.Accounts.Credential
 
   @doc """
   Returns the list of credentials.

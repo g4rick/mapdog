d821cd0bd80a151b6cafcaa4c51deade76376ddf
g4rick <285699093@qq.com>
2018-09-25 11:43:42 +0800

    重新定义用户相关表：用户基础信息表

---
+++ priv/repo/migrations/20180925024611_create_users.exs
@@ -0,0 +1,25 @@
+defmodule Mapdog.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  @doc """
+  用户基础表
+  包含基础信息，前端展示信息
+  不包含一切登录授权相关信息
+  """
+
+  def change do
+    create table(:users) do
+      add :user_id, :integer
+      add :nick, :string
+      add :avatar, :string
+      add :sex, :string
+      add :real_name, :string
+      add :age, :integer
+      add :state, :integer
+      add :state_des, :string
+      ass :last_login, :utc_datetime
+
+      timestamps()
+    end
+  end
+end

40227f5cd4de20fe844c8b6c10d179808e3833a3
g4rick <285699093@qq.com>
2018-09-30 17:28:10 +0800

    user_auth 添加 user_id关联

--- lib/mapdog/accounts/user_auth.ex
+++ lib/mapdog/accounts/user_auth.ex
@@ -1,13 +1,14 @@
 defmodule Mapdog.Accounts.UserAuth do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias Mapdog.Accounts.{User}
 
   schema "user_auths" do
     field :identity_type, :string
     field :identifier, :string
     field :access_token, :string
 
+    belongs_to :user_id, User
 
     timestamps()
   end
@@ -15,7 +16,7 @@ defmodule Mapdog.Accounts.UserAuth do
   @doc false
   def changeset(user_auth, attrs) do
     user_auth
-    |> cast(attrs, [])
-    |> validate_required([])
+    |> cast(attrs, [:identity_type, :identifier, :access_token])
+    |> validate_required([:identity_type, :identifier])
   end
 end
